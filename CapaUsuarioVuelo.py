08788fe5274b2f67d6ef3fac1372ffeefdf9041b
soportefrro <soportefrro@gmail.com>
2018-03-06 18:39:19 -0300

    Arreglamos entidad Reserva

--- CapaUsuarioVuelo.py
+++ CapaUsuarioVuelo.py
@@ -16,13 +16,14 @@ class CUVuelo():
        vp=Frame(self.ventana)
        vp.grid(column=0, row=0,  sticky=(N, S, E, W))
 
-       self.tree["columns"]=("dia_hora_salida","dia_hora_llegada","aerolinea","destino","capacidad")
+       self.tree["columns"]=("dia_hora_salida","dia_hora_llegada","aerolinea","destino","capacidad","precio")
        self.tree.column("#0", width=50)
        self.tree.column("dia_hora_salida", width=150)
        self.tree.column("dia_hora_llegada", width=150)
        self.tree.column("aerolinea", width=100)
        self.tree.column("destino", width=100)
        self.tree.column("capacidad", width=100)
+       self.tree.column("precio", width=100)
 
        self.tree.heading("#0", text="Nro vuelo")
        self.tree.heading("dia_hora_salida", text="Salida")
@@ -30,15 +31,16 @@ class CUVuelo():
        self.tree.heading("aerolinea", text="Aerolinea")
        self.tree.heading("destino", text="Destino")
        self.tree.heading("capacidad", text="Capacidad")
+       self.tree.heading("precio", text="Precio")
 
        lista= self.cnv.todosvuelo()
        for i in range(len(lista)):
-              self.tree.insert("", lista[i].nro_vuelo,text=lista[i].nro_vuelo, values=(lista[i].dia_hora_salida,lista[i].dia_hora_llegada,lista[i].aerolinea,lista[i].destino,lista[i].capacidad))
+              self.tree.insert("", lista[i].nro_vuelo,text=lista[i].nro_vuelo, values=(lista[i].dia_hora_salida,lista[i].dia_hora_llegada,lista[i].aerolinea,lista[i].destino,lista[i].capacidad,lista[i].precio))
 
        #Configuracion de la ventana:
        self.ventana.title("ABM Vuelos")
        self.ventana.resizable
-       self.ventana.geometry('650x400')
+       self.ventana.geometry('800x400')
        self.ventana.columnconfigure(0,weight=0)
        self.ventana.columnconfigure(1,weight=1)
        self.ventana.columnconfigure(2,weight=1)
@@ -66,8 +68,9 @@ class CUVuelo():
        d=self.aerolinea.get()
        e=self.destino.get()
        f=self.capacidad.get()
+       g=self.precio.get()
 
-       vuelo = Vuelo(nro_vuelo= a, dia_hora_salida= b, dia_hora_llegada= c,aerolinea =d,destino=e,capacidad=f)
+       vuelo = Vuelo(nro_vuelo= a, dia_hora_salida= b, dia_hora_llegada= c,aerolinea =d,destino=e,capacidad=f,precio=g)
        self.cnv.altavuelo(vuelo)
 
        tl=Toplevel()
@@ -96,6 +99,7 @@ class CUVuelo():
        self.aerolinea= StringVar()
        self.destino= StringVar()
        self.capacidad= IntVar()
+       self.precio = IntVar()
 
        botonagrega = Button(tl, text="Agregar",command=lambda: self.confirmaAlta())
        botonagrega.grid(column=2, row=3)
@@ -130,6 +134,12 @@ class CUVuelo():
        entradadni=Entry(vp, width= 20, textvariable= self.capacidad)
        entradadni.grid(column=1, row=5)
 
+       etiquetadni=Label(vp, text= "precio")
+       etiquetadni.grid(column=0, row=6)
+       entradadni=Entry(vp, width= 20, textvariable= self.precio)
+       entradadni.grid(column=1, row=6)
+
+
 
    def baja(self):
        posicion=self.tree.selection()
@@ -154,8 +164,9 @@ class CUVuelo():
        d=self.aerolinea.get()
        e=self.destino.get()
        f=self.capacidad.get()
+       g=self.precio.get()
 
-       v = Vuelo(nro_vuelo= var1, dia_hora_salida= var2, dia_hora_llegada= c,aerolinea =d,destino=e,capacidad=f)
+       v = Vuelo(nro_vuelo= var1, dia_hora_salida= var2, dia_hora_llegada= c,aerolinea =d,destino=e,capacidad=f,precio=g)
        self.cnv.modificar(v)
 
        # ventana de confirmación
@@ -182,6 +193,7 @@ class CUVuelo():
        self.aerolinea= StringVar()
        self.destino= StringVar()
        self.capacidad= IntVar()
+       self.precio = IntVar()
 
        posicion=self.tree.selection()
        var1=self.tree.item(posicion,"text")
@@ -194,6 +206,7 @@ class CUVuelo():
        self.aerolinea.set(v.aerolinea)
        self.destino.set(v.destino)
        self.capacidad.set(v.capacidad)
+       self.precio.set(v.precio)
 
        botonmodifica = Button(tl, text="Modificar",command=lambda: self.confirmaModificar(var1,var2))
        botonmodifica.grid(column=2, row=3)
@@ -229,11 +242,16 @@ class CUVuelo():
        entradadni=Entry(vp, width= 20, textvariable= self.capacidad)
        entradadni.grid(column=1, row=5)
 
+       etiquetadni=Label(vp, text= "precio")
+       etiquetadni.grid(column=0, row=6)
+       entradadni=Entry(vp, width= 20, textvariable= self.capacidad)
+       entradadni.grid(column=1, row=6)
+
    def refresh(self):
        [self.tree.delete(c) for c in self.tree.get_children()]
        lista= self.cnv.todosvuelo()
        for i in range(len(lista)):
-              self.tree.insert("", lista[i].nro_vuelo,text=lista[i].nro_vuelo, values=(lista[i].dia_hora_salida,lista[i].dia_hora_llegada,lista[i].aerolinea,lista[i].destino,lista[i].capacidad))
+              self.tree.insert("", lista[i].nro_vuelo,text=lista[i].nro_vuelo, values=(lista[i].dia_hora_salida,lista[i].dia_hora_llegada,lista[i].aerolinea,lista[i].destino,lista[i].capacidad,lista[i].precio))
 
 
 
